8903862629581f3200c63477ec63736ded9c97a2
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2023-03-10 01:33:21 +0900

    init

---
+++ src/main/scala/Playground/Playground.scala
@@ -0,0 +1,5 @@
+package Playground
+
+class Playground {
+
+}
---
+++ src/main/scala/Recap/Recap.scala
@@ -0,0 +1,9 @@
+package Recap
+
+object Recap extends App {
+  val bool: Boolean = false
+  println(bool)
+  val ifExpression = if(2>3) "bigger" else "smaller"
+  println(ifExpression)
+}
+
